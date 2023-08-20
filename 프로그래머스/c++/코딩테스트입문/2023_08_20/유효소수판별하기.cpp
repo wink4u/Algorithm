eb6fabba80b6a0c2815d884c38fbaf40ce1ba837
김승현 <kim0221_@naver.com>
2023-08-20 17:12:38 +0900

    0820 1차 commit

---
+++ 프로그래머스/c++/코딩테스트입문/2023_08_20/유효소수판별하기.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <vector>
+
+#include <iostream>
+
+using namespace std;
+
+// a, b의 최대공약수를 구하는 함수
+int divide(int a, int b){
+    if(a % b == 0) return b;
+    return divide(b, a % b);
+}
+
+int solution(int a, int b) {
+    
+    int answer = 0;
+    b /= divide(b, a);
+    
+    while(1){
+        if(b % 2 == 0) b /= 2;
+        
+        else if(b % 5 == 0) b /=5;
+        
+        else break;
+    }
+    
+    if(b == 1) answer = 1;
+    else answer = 2;
+    
+    return answer;
+}
