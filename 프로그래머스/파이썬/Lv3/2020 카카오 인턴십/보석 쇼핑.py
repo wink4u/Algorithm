164306fa63c28941cd541fa77adc7c4c7ea38345
김승현 <kim0221_@naver.com>
2024-01-01 23:53:00 +0900

    240101 3rd commit

--- 프로그래머스/파이썬/Lv3/2020 카카오 인턴십/보석 쇼핑.py
+++ 프로그래머스/파이썬/Lv3/2020 카카오 인턴십/보석 쇼핑.py
@@ -2,15 +2,16 @@ from collections import defaultdict
 
 def solution(gems):
     answer = [0, len(gems)]
-    
+    # 투포인터인데 start, end를 0기준으로 시작
     start, end = 0, 0
-    
+    # 보석의 개수
     gem_cnt = len(set(gems))
     gem_dict = {gems[0] : 1}
-
+    # start, end가 gems의 개수보다 적을때 while문
     while start < len(gems) and end < len(gems):
-        
+        # 보석을 다 찾았다면
         if len(gem_dict) == gem_cnt:
+            # start, end와 answer 기준과 비교
             if end - start < answer[1] - answer[0]:
                 answer = [start, end]
             else:
