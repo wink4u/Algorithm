de91740025954729961534f6acbae71c6211c98b
김승현 <kim0221_@naver.com>
2024-06-25 21:47:24 +0900

    240625 1st commit

--- 백준/Python/DP/Gold/자두나무.py
+++ 백준/Python/DP/Gold/자두나무.py
@@ -1,34 +1,28 @@
 import sys
-
 input = sys.stdin.readline
 
 T, W = map(int, input().split())
+DP = [[0 for _ in range(W + 1)] for _ in range(T + 1)]
+cnt = 0
 
-# 자두에 떨어지는 나무의 번호 1, 2
-# 움직일때 안움직일때 기준을 나누어서 값을 구해야할 것 같음
+for i in range(1, T + 1):
+    fruit = int(input())
 
-tree = [0]
-for _ in range(T):
-    tree.append(int(input()))
-
-DP = [[0] * (W + 1) for _ in range(T + 1)]
-
-for i in range(T + 1):
-
-    if tree[i] == 1:
-        DP[i][0] = DP[i - 1][0] + 1
+    if cnt > W:
+        cnt = W + 1
     else:
-        DP[i][0] = DP[i - 1][0]
+        cnt += 1
 
-    for j in range(1, W + 1):
-
-        if tree[i] == 1 and j % 2 == 0:
-            DP[i][j] = max(DP[i - 1][j - 1], DP[i - 1][j]) + 1
-        elif tree[i] == 2 and j % 2 == 1:
-            DP[i][j] = max(DP[i - 1][j - 1], DP[i - 1][j]) + 1
+    for j in range(cnt):
+        if j == 0:
+            DP[i][j] = DP[i - 1][j]
         else:
-            DP[i][j] = max(DP[i - 1][j - 1], DP[i - 1][j])
+            DP[i][j] += max(DP[i - 1][j], DP[i - 1][j - 1])
 
+    for j in range(W + 1):
+        if fruit == 1 and j % 2 == 0:
+            DP[i][j] += 1
+        elif fruit == 2 and j % 2:
+            DP[i][j] += 1
 
 print(max(DP[T]))
-
