5de08e56896a5d8c809562b76c65572a7c366d2a
김승현 <kim0221_@naver.com>
2024-10-17 17:00:37 +0900

    241017 4th commit

--- 백준/Python/유니온파인드/친구비.py
+++ 백준/Python/유니온파인드/친구비.py
@@ -7,11 +7,6 @@ n, m, k = map(int, input().split())
 parent = list(range(n))
 money = list(map(int, input().split()))
 
-def trans_parent(p, c):
-    for i in range(n):
-        if parent[i] == p:
-            parent[i] = c
-
 def find(a):
     if a == parent[a]:
         return a
@@ -25,32 +20,22 @@ def union(a, b):
 
     if money[pa] < money[pb]:
         parent[pb] = pa
-        if pb in parent:
-            trans_parent(pb, pa)
     else:
         parent[pa] = pb
-        if pa in parent:
-            trans_parent(pa, pb)
 
 for _ in range(m):
     a, b = map(int, input().split())
+    union(a - 1, b - 1)
 
-    if find(a - 1) != find(b - 1):
-        union(a - 1, b - 1)
+total = 0
+check = set()
 
-parent = list(set(parent))
-flag = 0
-total = k
+for i in range(n):
+    if find(i) not in check:
+        total += money[parent[i]]
+        check.add(parent[i])
 
-for i in range(len(parent)):
-    if total >= money[parent[i]]:
-        total -= money[parent[i]]
-    else:
-        flag = 1
-        break
-
-
-if flag:
-    print('Oh no')
+if total > k:
+    print("Oh no")
 else:
-    print(k - total)
+    print(total)
