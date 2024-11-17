9d3709f3423f33e3369d064705959306f6645a7f
김승현 <kim0221_@naver.com>
2024-11-17 20:42:38 +0900

    241117 4th commit

--- 백준/Python/DP/Gold/앱.py
+++ 백준/Python/DP/Gold/앱.py
@@ -8,21 +8,17 @@ unknown = [0] + list(map(int, input().split()))
 res = sum(unknown)
 DP = [[0 for _ in range(sum(unknown) + 1)] for _ in range(n + 1)]
 
-for i in range(1, n):
+for i in range(n + 1):
     byte = memory[i]
     cost = unknown[i]
 
-    for j in range(1, sum(unknown)):
+    for j in range(sum(unknown) + 1):
         if j < cost:
             DP[i][j] = DP[i - 1][j]
         else:
             DP[i][j] = max(byte + DP[i - 1][j - cost], DP[i - 1][j])
 
-
         if DP[i][j] >= m:
             res = min(res, j)
 
-if m != 0:
-    print(res)
-else:
-    print(0)
+print(res)
