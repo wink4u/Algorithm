56641a594e8bec1f870edac856a737a1143c28b8
김승현 <kim0221_@naver.com>
2026-04-23 22:36:52 +0900

    260423 1st commit

--- 백준/JavaScript/그리디/Silver/체인.js
+++ 백준/JavaScript/그리디/Silver/체인.js
@@ -1,23 +1,19 @@
 const fs = require("fs");
-const input = fs.readFileSync("test.txt").toString().trim().split("\n");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
 const N = Number(input[0])
 const arr = input[1].split(' ').map(Number)
 
-if (N <= 2) {
-    console.log(1)
-} else {
-    arr.sort((a, b) => a - b)
+arr.sort((a, b) => a - b)
 
-    let total = N
-    let cnt = 1
+let total = N
+let cnt = 1
 
-    for (let i = 0; i < N; i++){
-        if (cnt + arr[i] >= total) break
+for (let i = 0; i < N; i++){
+    if (cnt + arr[i] >= total) break
 
-        cnt += arr[i]
-        total--
-    }
-
-    console.log(cnt - 1)
+    cnt += arr[i]
+    total--
 }
+
+console.log(total - 1)
