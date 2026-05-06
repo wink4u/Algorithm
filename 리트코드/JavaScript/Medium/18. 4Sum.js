9de1dd2ea83472e6e37f86a268facf12853919eb
김승현 <kim0221_@naver.com>
2026-05-06 18:34:13 +0900

    250506 1st commit

---
+++ 리트코드/JavaScript/Medium/18. 4Sum.js
@@ -0,0 +1,38 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[][]}
+ */
+var fourSum = function(nums, target) {
+    const n = nums.length;
+    const ans = []
+    nums.sort((a, b) => a - b)
+
+    for (let i = 0; i < n - 3; i++){
+        if (i > 0 && nums[i] === nums[i - 1]) continue
+
+        for (let j = i + 1; j < n - 2; j++){
+            if (j > i + 1 && nums[j] === nums[j - 1]) continue
+
+            let left = j + 1
+            let right = n - 1
+
+            while (left < right) {
+                const sum = nums[i] + nums[j] + nums[left] + nums[right]
+
+                if (sum === target) {
+                    ans.push([nums[i], nums[j], nums[left], nums[right]])
+
+                    while (left < right && nums[left] === nums[left + 1]) left++
+                    while (left < right && nums[right] === nums[right - 1]) right--
+                
+                    left++
+                    right--
+                } else if (sum < target) left++
+                else right--
+            }
+        }
+    }
+
+    return ans
+};
