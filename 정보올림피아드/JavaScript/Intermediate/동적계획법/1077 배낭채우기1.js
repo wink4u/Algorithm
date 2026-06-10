7f7181ccb4a144df82ac1d1a9d3ba3609fc90ecb
김승현 <kim0221_@naver.com>
2026-06-10 23:54:36 +0900

    260610 1st commit

---
+++ 정보올림피아드/JavaScript/Intermediate/동적계획법/1077 배낭채우기1.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const input = fs.readFileSync('test.txt').toString().trim().split(/\r?\n/);
+
+const [N, W] = input[0].split(' ').map(Number)
+const arr = input.slice(1).map(v => v.split(' ').map(Number))
+
+const DP = Array(W + 1).fill(0)
+for (let i = 0; i < N; i++){
+    const [w, v] = arr[i]
+
+    for (let j = w; j <= W; j++){
+        DP[j] = Math.max(DP[j], DP[j - w] + v)
+    }
+}
+
+console.log(DP[W])
